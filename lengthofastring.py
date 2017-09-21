8d94050a7ca94e9aea3f13f98230f443c3279cdb
Amit Anand <anand.amit943@gmail.com>
2017-09-21 00:00:44 -0700

    Length of a string

---
+++ lengthofastring.py
@@ -0,0 +1,8 @@
+ # Write a Python program to calculate the length of a string ?
+
+import time
+
+n=str(input("Please enter a string :"))
+print("Calculating ...")
+time.sleep(1)
+print("Length :",len(n))
