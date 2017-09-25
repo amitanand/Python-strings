17ecbd34dbbfc4fdb6ce265e7d2f93d830fa1b58
Amit Anand <anand.amit943@gmail.com>
2017-09-24 22:34:48 -0700

    count the no of occurrence of each character

---
+++ characterfreq.py
@@ -0,0 +1,8 @@
+#  Write a Python program to count the number of characters (character frequency) in a string.?
+
+import time
+str_1=str(input("Please enter a string :"))
+a=0
+for a in str_1:
+    print(a,"is",str_1.count(a),"times")
+
