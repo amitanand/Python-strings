d8ff7082b84a965dcf1efc72b53bcfa3c675d0f5
Amit Anand <anand.amit943@gmail.com>
2017-09-24 22:36:37 -0700

    get the last part of string before a specified char

---
+++ lastpartofstrng.py
@@ -0,0 +1,8 @@
+''' Write a Python program to get the last part of a string before a specified character.
+https://www.w3resource.com/python-exercises'''
+
+import time
+
+string_1='https://www.w3resource.com/python-exercises/html'
+print(string_1.rsplit('/'))
+print(string_1.rsplit('-'))
