c67dbfc074e39b41ad306f6a0eebc648491b05f0
Amit Anand <anand.amit943@gmail.com>
2017-09-25 20:37:59 -0700

    reverse the specified string

---
+++ reversestring.py
@@ -0,0 +1,9 @@
+# Write a Python program to reverse a string. ?
+
+import time
+
+def reverse_string(string):
+    print(''.join(reversed(string)))
+
+reverse_string('amit anand')
+reverse_string('motilal nehru')
