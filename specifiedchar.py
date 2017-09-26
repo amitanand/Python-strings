d1a24036bfd25c709805ffdaf4507bd03eb6a170
Amit Anand <anand.amit943@gmail.com>
2017-09-25 20:25:37 -0700

    check whether a string starts with a specified char

---
+++ specifiedchar.py
@@ -0,0 +1,6 @@
+# Write a Python program to check whether a string starts with specified characters. ?
+
+import time
+
+str_1=str(input("Please enter the string :"))
+print(str_1.startswith('https://'))
